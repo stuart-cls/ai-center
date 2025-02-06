059eb31c9eccf92b9dd8800f8daf487e6e44f016
Stuart Read <stuart.read@lightsource.ca>
2025-02-06 11:58:03 -0600

    Move object-detection-specific code into net module

--- aicenter/__init__.py
+++ aicenter/__init__.py
@@ -1,19 +1,14 @@
-from collections import namedtuple, defaultdict
-
 import cv2
 import numpy
 
 from aicenter import utils
 from aicenter.log import get_module_logger
-from aicenter.net import load_model
+from aicenter.net import load_model, Result
 from aicenter.sam import TrackingSAM
 
 logger = get_module_logger(__name__)
 
-# Result Type
-Result = namedtuple('Result', 'type x y w h score')
-
-CONF_THRESH, NMS_THRESH = 0.125, 0.25
+CONF_THRESH, NMS_THRESH = 0.25, 0.25
 
 
 class AiCenter:
@@ -24,11 +19,8 @@ class AiCenter:
         self.model_path = model
 
         # prepare neural network for detection
-        self.net = load_model(model, CONF_THRESH)
-        self.net.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-        self.net.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-        self.layers = self.net.net.getLayerNames()
-        self.output_layers = self.net.net.getUnconnectedOutLayersNames()
+        self.net = load_model(model, CONF_THRESH, NMS_THRESH)
+
         # setup SAM2 for segmentation
         self.sam = TrackingSAM()
 
@@ -42,27 +34,6 @@ class AiCenter:
             return
         else:
             return frame
-
-    def process_results(self, width, height, outputs):
-        class_ids, confidences, bboxes = [], [], []
-        for output in outputs:
-            for bb, conf, cid in self.net.parse_output(output, width=width, height=height):
-                bboxes.append(bb)
-                confidences.append(conf)
-                class_ids.append(cid)
-
-        if bboxes:
-            results = defaultdict(list)
-            indices = cv2.dnn.NMSBoxes(bboxes, confidences, CONF_THRESH, NMS_THRESH).flatten()
-            nms_boxes = [(bboxes[i], confidences[i], class_ids[i]) for i in indices]
-            for bbox, score, class_id in nms_boxes:
-                x, y, w, h = bbox
-                label = self.net.names[class_id]
-                logger.debug(f'{label} found at: {x} {y} [{w} {h}], prob={score}')
-                results[label].append(Result(label, x, y, w, h, score))
-            for label, llist in results.items():
-                results[label] = sorted(llist, key=lambda result: result.score, reverse=True)
-            return results
 
     @staticmethod
     def process_features(frame):
@@ -85,10 +56,8 @@ class AiCenter:
     def process_frame(self, frame):
         if frame is not None:
             height, width = frame.shape[:2]
-            blob = cv2.dnn.blobFromImage(frame, 0.00392, (self.net.size, self.net.size), swapRB=True, crop=False)
-            self.net.net.setInput(blob)
-            outputs = self.net.net.forward(self.output_layers)
-            results = self.process_results(width, height, outputs)
+            outputs = self.net.predict(frame)
+            results = self.net.process_results(width, height, outputs)
             if not results:
                 # attempt regular image processing
                 results = self.process_features(frame)
--- aicenter/net.py
+++ aicenter/net.py
@@ -1,4 +1,5 @@
 import os.path
+from collections import defaultdict, namedtuple
 from pathlib import Path
 from typing import Iterator
 
@@ -6,25 +7,66 @@ import cv2
 import numpy
 import yaml
 
+from aicenter.log import get_module_logger
+
+logger = get_module_logger(__name__)
+
+# Result Type
+Result = namedtuple('Result', 'type x y w h score')
 
 class Net:
     size = None
     net = None
     names = None
 
-    def __init__(self, model_path, conf_thres):
+    def __init__(self, model_path, conf_threshold: float, nms_threshold: float):
         self.model_path = model_path
-        self.conf_thres = conf_thres
+        self.conf_threshold = conf_threshold
+        self.nms_threshold = nms_threshold
+        self.load_model()
+        self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+        self.output_layers = self.net.getUnconnectedOutLayersNames()
 
-    def parse_output(self, output, width, height) -> Iterator[tuple[list[int], float, int]]:
+    def load_model(self):
+        """Model-specific loading"""
         raise NotImplementedError
 
+    def parse_output(self, output, width, height) -> Iterator[tuple[list[int], float, int]]:
+        """Model-specific output handling"""
+        raise NotImplementedError
+
+    def predict(self, image: numpy.ndarray) -> numpy.ndarray:
+        blob = cv2.dnn.blobFromImage(image, 0.00392, (self.size, self.size), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        return self.net.forward(self.output_layers)
+
+    def process_results(self, width, height, outputs):
+        class_ids, confidences, bboxes = [], [], []
+        for output in outputs:
+            for bb, conf, cid in self.parse_output(output, width=width, height=height):
+                bboxes.append(bb)
+                confidences.append(conf)
+                class_ids.append(cid)
+
+        if bboxes:
+            results = defaultdict(list)
+            indices = cv2.dnn.NMSBoxes(bboxes, confidences, self.conf_threshold, self.nms_threshold).flatten()
+            nms_boxes = [(bboxes[i], confidences[i], class_ids[i]) for i in indices]
+            for bbox, score, class_id in nms_boxes:
+                x, y, w, h = bbox
+                label = self.names[class_id]
+                logger.debug(f'{label} found at: {x} {y} [{w} {h}], prob={score}')
+                results[label].append(Result(label, x, y, w, h, score))
+            for label, llist in results.items():
+                results[label] = sorted(llist, key=lambda result: result.score, reverse=True)
+            return results
 
 class DarkNet(Net):
     size = 416
 
-    def __init__(self, model_path, conf_thres):
-        super().__init__(model_path, conf_thres)
+    def load_model(self):
+        model_path = str(self.model_path)
         with open(os.path.join(model_path, 'yolov3.names'), 'r', encoding='utf-8') as fobj:
             self.names = [line.strip() for line in fobj.readlines()]
         self.net = cv2.dnn.readNetFromDarknet(
@@ -38,7 +80,7 @@ class DarkNet(Net):
             class_id = numpy.argmax(scores)
             confidence = scores[class_id]
 
-            if confidence > self.conf_thres:
+            if confidence > self.conf_threshold:
                 cx, cy, w, h = (detection[0:4] * numpy.array([width, height, width, height])).astype(int)
                 x = int(cx - w / 2)
                 y = int(cy - h / 2)
@@ -49,9 +91,8 @@ class DarkNet(Net):
 class ONNXNet(Net):
     size = 640
 
-    def __init__(self, model_path, conf_thres):
-        super().__init__(model_path, conf_thres)
-        self.model_path = Path(model_path)
+    def load_model(self):
+        self.model_path = Path(self.model_path)
         with open(next(self.model_path.glob('*.yaml')), 'r') as fobj:
             data = yaml.safe_load(fobj)
         self.names = list(data['names'].values())
@@ -65,7 +106,7 @@ class ONNXNet(Net):
             class_id = numpy.argmax(scores)
             confidence = scores[class_id]
 
-            if confidence > self.conf_thres:
+            if confidence > self.conf_threshold:
                 scale = numpy.array([width, height, width, height]) / self.size
                 cx, cy, w, h = (detection[0:4] * scale).astype(int)
                 x = int(cx - w / 2)
@@ -74,10 +115,10 @@ class ONNXNet(Net):
                 yield [x, y, int(w), int(h)], float(confidence), int(class_id)
 
 
-def load_model(model_path: str or Path, conf_thres: float) -> Net:
+def load_model(model_path: str or Path, conf_threshold: float, nms_threshold: float) -> Net:
     for n in [DarkNet, ONNXNet]:
         try:
-            net = n(model_path, conf_thres)
+            net = n(model_path, conf_threshold, nms_threshold)
         except OSError:
             continue
         else:
