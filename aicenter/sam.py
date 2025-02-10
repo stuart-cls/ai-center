7e8f9c3d33f8c666814492ca0a8610a6f35984af
Stuart Read <stuart.read@lightsource.ca>
2025-02-10 16:49:12 -0600

    Multi-object tracking

--- aicenter/sam.py
+++ aicenter/sam.py
@@ -65,31 +65,33 @@ class SAM2:
         return masks, scores
 
     @staticmethod
-    def process_results(masks, scores, label):
-        # TODO this should probably return the same type of thing as Net.process_results
-        results = []
+    def process_results(masks_list, scores_list, labels: list[str]):
+        results = defaultdict(list)
         # if only one mask result, not enough dimensions
-        masks = numpy.array(masks, ndmin=4, copy=False)
-        if masks.size:
-            for mask, score in zip(masks, scores):
-                mask = mask.squeeze(0)
-                score = score.item()
-                contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-                max_contour = max(contours, key=cv2.contourArea) if len(contours) > 0 else None
-                if max_contour is not None:
-                    # Calculate image moments of the detected contour
-                    moments = cv2.moments(max_contour)
-                    try:
-                        x_centroid = round(moments['m10'] / moments['m00'])
-                        y_centroid = round(moments['m01'] / moments['m00'])
-                    except ZeroDivisionError:
-                        x_centroid = None
-                        y_centroid = None
-                    else:
-                        logger.debug(f"Segmentation mask centroid: {x_centroid}, {y_centroid}")
-                    x, y, w, h = cv2.boundingRect(max_contour)
-                    logger.debug(f'{label} found at: {x} {y} [{w} {h}], prob={score:.2f}')
-                    results.append(MaskResult(label, x, y, w, h, score, x_centroid, y_centroid, mask, contours))
+        for masks, scores, label in zip(masks_list, scores_list, labels):
+            masks = numpy.array(masks, ndmin=4, copy=False)
+            if masks.size:
+                for mask, score in zip(masks, scores):
+                    mask = mask.squeeze(0)
+                    score = score.item()
+                    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                    max_contour = max(contours, key=cv2.contourArea) if len(contours) > 0 else None
+                    if max_contour is not None:
+                        # Calculate image moments of the detected contour
+                        moments = cv2.moments(max_contour)
+                        try:
+                            x_centroid = round(moments['m10'] / moments['m00'])
+                            y_centroid = round(moments['m01'] / moments['m00'])
+                        except ZeroDivisionError:
+                            x_centroid = None
+                            y_centroid = None
+                        else:
+                            logger.debug(f"Segmentation mask centroid: {x_centroid}, {y_centroid}")
+                        x, y, w, h = cv2.boundingRect(max_contour)
+                        logger.debug(f'{label} found at: {x} {y} [{w} {h}], prob={score:.2f}')
+                        results[label].append(MaskResult(label, x, y, w, h, score, x_centroid, y_centroid, mask, contours))
+        for label, llist in results.items():
+            results[label] = sorted(llist, key=lambda result: result.score, reverse=True)
         return results
 
 @dataclass
@@ -149,47 +151,59 @@ class TrackingSAM(SAM2):
         logger.debug(f"Added new tracked {label} object, {len(self.tracked_objects)} total")
 
     def track_objects(self, image: numpy.ndarray, results: dict[list[Result]], width, height):
+        if not self.predictor:
+            return
         norm = numpy.array([width, height, width, height])
         for label, objects in results.items():
-            if label == 'loop' and objects and self.predictor and not self.loop:
+            if label == 'loop' and objects and not self.loop:
                 # Only use the highest-scoring loop as the prompt
                 loop = objects[0]
                 xyxy = [loop.x, loop.y, loop.x + loop.w, loop.y + loop.h]
                 input_boxes = numpy.atleast_2d(numpy.array(xyxy))
                 self.track_input_boxes(image, input_boxes, norm, label)
+            elif label != 'loop' and objects:
+                xyxy = [[o.x, o.y, o.x + o.w, o.y + o.h] for o in objects]
+                input_boxes = numpy.atleast_2d(numpy.array(xyxy))
+                self.track_input_boxes(image, input_boxes, norm, label)
 
     def predict(self, image: numpy.ndarray):
-        # Select current object
-        tracked_object = self.tracked_objects[0]
-        # Process video frames with model
-        t1 = time.perf_counter()
-        encoded_imgs_list, _, _ = self.predictor.encode_image(image)
-        obj_score, mask_pred, mem_enc, obj_ptr = self.predictor.step_video_masking(
-            encoded_imgs_list, *tracked_object.video_masking_inputs,
-        )
-        t2 = time.perf_counter()
-        logger.debug(f"Inference took {round(1000 * (t2 - t1))} ms, score={obj_score[0][0]:.2f}")
+        masks = []
+        scores = []
+        labels = []
+        for tracked_object in self.tracked_objects:
+            # Process video frames with model
+            t1 = time.perf_counter()
+            encoded_imgs_list, _, _ = self.predictor.encode_image(image)
+            obj_score, mask_pred, mem_enc, obj_ptr = self.predictor.step_video_masking(
+                encoded_imgs_list, *tracked_object.video_masking_inputs,
+            )
+            t2 = time.perf_counter()
+            logger.debug(f"Inference took {round(1000 * (t2 - t1))} ms, score={obj_score[0][0]:.2f}")
 
-        # Store object results for future frames
-        if obj_score < 0:
-            # TODO Need to keep object for occlusion support, maybe drop when scores stay low for a long time
-            logger.debug("Bad object score! Implies broken tracking! Dropping tracked object.")
-            self.tracked_objects.remove(tracked_object)
-            self.init = False
-            return [], []
-        tracked_object.prev_memory_encodings.appendleft(mem_enc)
-        tracked_object.prev_object_pointers.appendleft(obj_ptr)
+            # Store object results for future frames
+            if obj_score < 0:
+                # TODO Need to keep object for occlusion support, maybe drop when scores stay low for a long time
+                logger.debug("Bad object score! Implies broken tracking! Dropping tracked object.")
+                self.tracked_objects.remove(tracked_object)
+                self.init = False
+                return [], []
+            tracked_object.prev_memory_encodings.appendleft(mem_enc)
+            tracked_object.prev_object_pointers.appendleft(obj_ptr)
 
-        # Create mask for display
-        dispres_mask = torch.nn.functional.interpolate(
-            mask_pred,
-            size=image.shape[0:2],
-            mode="bilinear",
-            align_corners=False,
-        )
-        disp_mask = ((dispres_mask > 0.0).byte() * 255).cpu().numpy().squeeze()
+            # Create mask for display
+            dispres_mask = torch.nn.functional.interpolate(
+                mask_pred,
+                size=image.shape[0:2],
+                mode="bilinear",
+                align_corners=False,
+            )
+            disp_mask = ((dispres_mask > 0.0).byte() * 255).cpu().numpy().squeeze()
 
-        return disp_mask, obj_score
+            masks.append(disp_mask)
+            scores.append(obj_score)
+            labels.append(tracked_object.label)
+
+        return masks, scores, labels
 
 
 def show_masks(image, masks):
